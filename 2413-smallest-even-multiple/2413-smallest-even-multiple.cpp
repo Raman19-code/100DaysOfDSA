4cd0893b78fc5545bb3bc5b650c31e38fd934f90
Raman19-code <ramndeepkaur005@gmail.com>
2026-08-02 15:49:36 +0530

    Time: 0 ms (100%), Space: 7.8 MB (95.73%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        int ans=0;
-        for(int i=1;i<=1000;i++)
+        if(n%2==0)
         {
-            if(i%2==0 && i%n==0)
-            {
-                ans=i;
-                break;
-            }
+            return n;
         }
-        return ans;
+        return 2*n;
         
     }
 };
