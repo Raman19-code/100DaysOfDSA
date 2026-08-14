b53290a52c485c11618197ee8fd6c2fb74720d70
Raman19-code <ramndeepkaur005@gmail.com>
2026-08-14 17:44:30 +0530

    Time: 5 ms (57.81%), Space: 11.8 MB (25.49%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> ans;
-        int n = s.size(), k = p.size();
-        if (n < k) return ans;
-
-        int freqP[26] = {};
-        int freqWindow[26] = {};
-
-        for (int i = 0; i < k; i++)
+        vector<int>ans;
+        int pfreq[256]={};
+        int sfreq[256]={};
+        int s1=s.size();
+        int p1=p.size();
+        if(p1>s1)
         {
-            freqP[p[i] - 'a']++;
-            freqWindow[s[i] - 'a']++;
+            return {};
         }
-
-        if (equal(begin(freqP), end(freqP), begin(freqWindow)))
+        for(int i=0;i<p1;i++)
+        {
+            pfreq[p[i]-'a']++;
+        }
+        for(int i=0;i<p1;i++)
+        {
+            sfreq[s[i]-'a']++;
+        }
+        if(memcmp(pfreq,sfreq,sizeof(pfreq))==0)
         {
             ans.push_back(0);
         }
-
-        for (int i = k; i < n; i++)
+        for(int i=p1;i<s1;i++)
         {
-            freqWindow[s[i] - 'a']++;          // add incoming char
-            freqWindow[s[i - k] - 'a']--;      // remove outgoing char
-
-            if (equal(begin(freqP), end(freqP), begin(freqWindow)))
+            sfreq[s[i]-'a']++;
+            sfreq[s[i-p1]-'a']--;
+            if(memcmp(pfreq,sfreq,sizeof(pfreq))==0)
             {
-                ans.push_back(i - k + 1);
+                ans.push_back(i-p1+1);
             }
         }
         return ans;
+        
     }
 };
