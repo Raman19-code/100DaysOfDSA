8e3ac1aaa1d7e8575721f90d074a4d8865060830
Raman19-code <ramndeepkaur005@gmail.com>
2026-08-30 00:09:20 +0530

    Time: 3 ms (17.96%), Space: 8.9 MB (56.95%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        
-        int i = a.size() - 1;
-        int j = b.size() - 1;
-        
-        int carry = 0;
-        
-        string ans = "";
-        
-        while(i >= 0 || j >= 0 || carry) {
-            
-            int digit1 = 0;
-            int digit2 = 0;
-            
-            if(i >= 0) {
-                digit1 = a[i] - '0';
+        int i =a.size()-1;
+        int j=b.size()-1;
+        int carry=0;
+        string result="";
+        while(i>=0 || j>=0 || carry)
+        {
+            int digit1=0;
+            int digit2=0;
+            if(i>=0)
+            {
+                digit1=a[i]-'0';   
             }
-            
-            if(j >= 0) {
-                digit2 = b[j] - '0';
+            if(j>=0)
+            {
+                digit2=b[j]-'0';
             }
-            
-            int sum = digit1 + digit2 + carry;
-            
-            int digit = sum % 2;
-            carry = sum / 2;
-            
-            ans += (digit + '0');
-            
+           int  sum=digit1+digit2+carry;
+            int digit=sum%2;
+            carry=sum/2;
+            result+=(digit+'0');
             i--;
             j--;
         }
+        reverse(result.begin(),result.end());
+        return result;
         
-        reverse(ans.begin(), ans.end());
-        
-        return ans;
     }
 };
