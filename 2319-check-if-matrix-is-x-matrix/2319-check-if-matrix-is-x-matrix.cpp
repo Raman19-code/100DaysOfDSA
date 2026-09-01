124c4e585de1b0c1c7bbbafca67dd93c9663fb90
Raman19-code <ramndeepkaur005@gmail.com>
2026-09-01 15:52:50 +0530

    Time: 0 ms (100%), Space: 19.9 MB (82.83%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(i == j || i + j == n - 1) {
+                    // Diagonal element must be non-zero
+                    if(grid[i][j] == 0)
+                        return false;
+                }
+                else {
+                    // Non-diagonal element must be zero
+                    if(grid[i][j] != 0)
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
